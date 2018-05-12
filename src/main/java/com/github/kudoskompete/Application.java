20647e3c065c44dd476c13f371c00a4c7797f3d4
Andrea Naylor <andi.goldsworthy@gmail.com>
2018-05-12 14:42:53 +0100

    revert to Spring MVC

--- src/main/java/com/github/kudoskompete/Application.java
+++ src/main/java/com/github/kudoskompete/Application.java
@@ -2,11 +2,6 @@ package com.github.kudoskompete;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import spark.ModelAndView;
-import spark.template.velocity.VelocityTemplateEngine;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static spark.Spark.get;
 
@@ -15,18 +10,6 @@ public class Application {
 
     public static void main(String[] args) {
         Logger.getRootLogger().setLevel(Level.OFF);
-
         get("/hello", (req, res) -> "Hello World");
-
-//        // do this
-//        get("/login", (req, res) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            return new VelocityTemplateEngine().render(
-//                    new ModelAndView(model, "login.vtl")
-//            );
-//        });
-        get("/login", (req, res) -> {
-            return new ModelAndView(new HashMap<>(), "templates/login.vtl");
-        }, new VelocityTemplateEngine());
     }
 }
--- src/main/java/com/github/kudoskompete/controllers/HelloWorldController.java
+++ src/main/java/com/github/kudoskompete/controllers/HelloWorldController.java
@@ -1,21 +1,10 @@
 package com.github.kudoskompete.controllers;
 
 
-import spark.Request;
-import spark.Response;
-import spark.Route;
-
 public class HelloWorldController {
 
 
     public String hello() {
         return "Hello World";
     }
-
-//    public static Route login(Request request, Response response) {
-//
-//    }
-
-
-
 }
