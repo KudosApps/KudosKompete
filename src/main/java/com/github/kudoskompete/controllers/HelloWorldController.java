6455e11baa4f3c390765a4ec2ecadb9a896ae676
Andrea Naylor <andi.goldsworthy@gmail.com>
2018-05-12 14:51:49 +0100

    second revert to Spring MVC

--- src/main/java/com/github/kudoskompete/controllers/HelloWorldController.java
+++ src/main/java/com/github/kudoskompete/controllers/HelloWorldController.java
@@ -1,10 +1,21 @@
 package com.github.kudoskompete.controllers;
 
 
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
 public class HelloWorldController {
 
 
     public String hello() {
         return "Hello World";
     }
+
+//    public static Route login(Request request, Response response) {
+//
+//    }
+
+
+
 }
